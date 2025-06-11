0ce68996d2302ecb2164dcce42f4cd612243409d
Khaly DIENG <kd.univ@sensor6ty.com>
2025-06-11 16:03:05 +0200

    refactor: update ConvertBack method to include TODO comment and clarify exception handling

--- src/Soditech.IntelPrev.Mobile/Converters/FieldTemplateSelector.cs
+++ src/Soditech.IntelPrev.Mobile/Converters/FieldTemplateSelector.cs
@@ -42,7 +42,8 @@ namespace Soditech.IntelPrev.Mobile.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            //TODO: Not yet implemented
+            throw new NotSupportedException("ConvertBack is not supported for this converter.");
         }
     }
 }
